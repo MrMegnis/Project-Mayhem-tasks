890625995c3ffacbd1b6f3515fe499b00ae9a8a4
L33t <maxim.y.mischenko@gmail.com>
2023-12-14 00:14:15 +0300

    add methods annotations

--- utils/database/db_worker.py
+++ utils/database/db_worker.py
@@ -9,7 +9,7 @@ settings = get_settings('.env')
 
 
 class DBWorker:
-    def __init__(self, db_path=None):
+    def __init__(self, db_path: str = None) -> None:
         self.session: Session | None = None
         if db_path is None:
             self.engine = create_engine(f'{settings.database.path}/{settings.database.board_games}')
@@ -33,7 +33,7 @@ class DBWorker:
         return wrapper
 
     @with_session
-    def add_boardgame(self, name: str, description: str = None, max_players: int = None):
+    def add_boardgame(self, name: str, description: str = None, max_players: int = None) -> None:
         if self.get_boardgame(name):
             logger.info(f'Board game is already in the table')
             return
@@ -43,11 +43,11 @@ class DBWorker:
         logger.info(f'Board game {name} successfully added to database!')
 
     @with_session
-    def get_boardgame(self, name: str):
+    def get_boardgame(self, name: str) -> BoardGame | None:
         return self.session.query(BoardGame).filter_by(name=name).first()
 
     @with_session
-    def update_boardgame(self, boardgame_id, description: str, max_players: int):
+    def update_boardgame(self, boardgame_id: int, description: str, max_players: int) -> None:
         self.session.query(BoardGame).filter_by(id=boardgame_id).update(
             {BoardGame.description: description, BoardGame.max_players: max_players})
         self.session.commit()
@@ -55,6 +55,6 @@ class DBWorker:
             f'Board game with id: {boardgame_id} updated max amount of players to {max_players} and description to {description}')
 
     @with_session
-    def remove_boardgame(self, name):
+    def remove_boardgame(self, name: str) -> None:
         self.session.query(BoardGame).filter_by(name=name).delete()
         self.session.commit()
